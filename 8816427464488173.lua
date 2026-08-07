e105c105fe629a23b4598d0556f5efa364b1a933
Beshoy966 <hanyfekk@gmail.com>
2026-08-07 20:37:27 +0300

    Zythera-X Blade Ball

--- 8816427464488173.lua
+++ 8816427464488173.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://xhider.xyz/raw/Lovely/BladeBallScriptAzure"))() 
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/56788bdcf8c29d3de435a05535d0b710a821b2c1051d909b3037a4a1d6d464ae/download"))()
